afe8c334f675b6e28c4e99ce230c6c8e72dd8a9b
squareleaf <tiffany.green@wellbe.me>
2017-11-15 22:36:17 -0600

    Grab all paginated results and concatenate into one result set

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,7 +1,22 @@
 class PullRequestsController < ApplicationController
   def index
-    response = HTTParty.get("https://api.github.com/repos/lodash/lodash/pulls?state=all")
+    response = HTTParty.get("https://api.github.com/repos/lodash/lodash/pulls?state=all&per_page=100")
 
-    @response = response.parsed_response
+    headers = response.headers
+
+    # grab the number of the last page from the link param of the headers hash
+    if headers["link"].present?
+      index_last_page = headers["link"].rindex('&page=')
+      last_page = headers["link"].slice(index_last_page + 6).to_i
+    end
+
+    # concatenate results
+    @response = []
+    for i in 1..last_page
+      response = HTTParty.get("https://api.github.com/repos/lodash/lodash/pulls?state=all&page=#{i}&per_page=100")
+      @response << response.parsed_response
+    end
+
+    @response = @response.flatten
   end
 end
